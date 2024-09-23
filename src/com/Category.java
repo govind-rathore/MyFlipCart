eb736833a2bc0a0f1bef813ebe155c0da4ee9daa
Govind <rathore.govind2000@gmail.com>
2024-09-24 00:46:34 +0530

    adding category

--- src/com/Category.java
+++ src/com/Category.java
@@ -1,5 +1,17 @@
 package com;
 
 public class Category {
+    private String name;
+    private String description;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 
 }
+
